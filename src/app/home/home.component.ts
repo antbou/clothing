5654a8a693a82694673fec75b4bfc0d97f3d37dd
Antho <bouillant.anthony@gmail.com>
2024-05-03 13:42:45 +0200

    Add paginator

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,11 +3,12 @@ import { ProductsService } from '../services/products.service';
 import { Product, Products } from '../types';
 import { ProductComponent } from '../components/product/product.component';
 import { CommonModule } from '@angular/common';
+import { PaginatorModule } from 'primeng/paginator';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ProductComponent, CommonModule],
+  imports: [ProductComponent, CommonModule, PaginatorModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
@@ -16,15 +17,40 @@ export class HomeComponent {
 
   products: Product[] = [];
 
+  first: number = 0;
+  rows: number = 5;
+  totalRecords: number = 0;
+
   onProductOutput(product: Product) {
     console.log(product);
   }
 
-  ngOnInit() {
+  onPageChange(event: any) {
+    this.fetchProducts(event.page, event.rows);
+  }
+
+  fetchProducts(page: number, perPage: number) {
     this.productsService
-      .getProducts('http://localhost:3000/clothes', { page: 0, perPage: 5 })
+      .getProducts('http://localhost:3000/clothes', { page, perPage })
       .subscribe((products: Products) => {
         this.products = products.items;
+        this.totalRecords = products.total;
       });
+  }
+
+  editProduct(product: Product) {
+    console.log('Editing product:', product);
+  }
+
+  deleteProduct(product: Product) {
+    console.log('Deleting product:', product);
+  }
+
+  addProduct(product: Product) {
+    console.log('Adding product:', product);
+  }
+
+  ngOnInit() {
+    this.fetchProducts(0, this.rows);
   }
 }
